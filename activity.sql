06b7e7d3cf8cedf02ffca5fe3d592800fe470dd8
stepantishhen <stepantishhen@mail.ru>
2023-10-23 20:46:48 +0300

    add files

--- activity.sql
+++ activity.sql
@@ -1,1 +1,14 @@
----
+WITH RECURSIVE EntityHierarchy AS (
+  SELECT P.ActivityId, A.ActivityTypeId, A.Name, 1 AS Level
+  FROM Program P
+  INNER JOIN Activity A ON P.ActivityId = A.ActivityId
+  
+  UNION ALL
+  
+  SELECT A.ActivityId, A.ActivityTypeId, A.Name, EH.Level + 1
+  FROM Activity A
+  INNER JOIN EntityHierarchy EH ON A.ParentId = EH.ActivityId
+)
+SELECT *
+FROM EntityHierarchy
+ORDER BY Level, ActivityId;
--- data.sql
+++ data.sql
@@ -1,17 +1,41 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+INSERT INTO ActivityType (ActivityTypeId, Name)
+VALUES (1, 'Концерт'),
+       (2, 'Мастер класс'),
+       (3, 'Выставка');
+
+INSERT INTO Activity (ActivityId, ParentId, ActivityTypeId, Name, AreaId)
+VALUES (1, NULL, 1, 'Linken park', 1),
+       (2, 1, 2, 'Street art', 2),
+       (3, 1, 2, 'Лепка', 3),
+       (4, 2, 1, 'Neffex', 1),
+       (5, 2, 3, 'Ван Гог', 3),
+       (6, NULL, 3, 'Современное искуство', 2);
+
+INSERT INTO Program (ActivityId)
+VALUES (1),
+       (2),
+       (3),
+       (6);
+
+INSERT INTO SubProgram (ActivityId)
+VALUES (4),
+       (5);
+
+INSERT INTO Project (ActivityId)
+VALUES (2),
+       (3),
+       (4),
+       (5);
+
+INSERT INTO Contract (ActivityId, AreaId)
+VALUES (3, 1),
+       (4, 2),
+       (6, 3);
+
+INSERT INTO Point (ActivityId)
+VALUES (1),
+       (2),
+       (3),
+       (4),
+       (5),
+       (6);
